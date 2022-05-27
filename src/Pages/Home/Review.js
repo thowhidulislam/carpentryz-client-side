c9e21ecd3462f3b823836593eedf64d67f707de9
thowhidulislam <thowhid.ndc14@gmail.com>
2022-05-27 20:47:00 +0600

    review displaying on home page

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Rating from 'react-rating';
+import starYellow from '../../assets/icons/star-yellow.png';
+import starGrey from '../../assets/icons/star-grey.png';
 
-const Review = () => {
+
+const Review = ({ review }) => {
+    const { message, user, rating } = review
     return (
-        <div>
-            <h1>Review</h1>
+        <div class="card max-w-md bg-base-100 shadow-xl">
+            <div class="card-body">
+                <div class="avatar">
+                    <div class="w-24 rounded-full mx-auto">
+                        <img src={user?.photoURL} alt="" />
+                    </div>
+                </div>
+                <div className='text-center'>
+                    <p className='font-bold'>{user?.displayName}</p>
+                </div>
+                <div className='mx-auto'>
+                    <Rating
+                        placeholderRating={rating}
+                        emptySymbol={<img src={starGrey} className="icon w-[30px]" alt='' />}
+                        placeholderSymbol={<img src={starYellow} className="icon w-[30px]" alt='' />}
+                        readonly
+                    />
+                </div>
+                <p>{message}</p>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,13 +1,25 @@
-import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading/Loading';
+import Review from './Review';
 
 const Reviews = () => {
-    axios.get('http://localhost:5000/review').then(function (response) {
-        console.log(response)
-    })
+    const { data: homeReviews, isLoading } = useQuery('homeReviews', () => fetch('http://localhost:5000/review').then(res => res.json()))
+    console.log(homeReviews)
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
-        <div>
-            <h1>Reviews</h1>
+        <div className=' px-24'>
+            <h1 className='text-4xl font-bold text-center '>Reviews</h1>
+            <div className='grid grid-cols-3 gap-6 my-6'>
+                {
+                    homeReviews.result.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
+            </div>
         </div>
     );
 };
